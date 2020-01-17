9fa017603fad1ac96ee0809f81a3546ba3e741ca
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-01-17 14:58:04 -0300

    Deletando arquivos

--- src/main/java/com/jopaulo/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/jopaulo/socialbooks/resources/LivrosResources.java
@@ -3,6 +3,8 @@ package com.jopaulo.socialbooks.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +13,29 @@ import com.jopaulo.socialbooks.domain.Livro;
 import com.jopaulo.socialbooks.repository.LivrosRepository;
 
 @RestController
+@RequestMapping("/livros")
 public class LivrosResources {
 	
 	@Autowired
 	private LivrosRepository livrosRepository;
 	
-	@RequestMapping(value = "/livros", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Livro> listar() {
 		return livrosRepository.findAll(); //busca todos
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void salvar(@RequestBody Livro livro) {
+		livrosRepository.save(livro);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Livro buscar(@PathVariable("id") Long id) {
+		return livrosRepository.findById(id).orElse(null);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void deletar(@PathVariable("id")Long id) {
+		livrosRepository.deleteById(id);
 	}
 }
