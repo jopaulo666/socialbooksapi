0088710de76c01457706b95f1d439d2f613f133a
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-01-17 00:48:51 -0300

    Criação da classe Livro

--- src/main/java/com/jopaulo/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/jopaulo/socialbooks/resources/LivrosResources.java
@@ -1,14 +1,26 @@
 package com.jopaulo.socialbooks.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jopaulo.socialbooks.domain.Livro;
 
 @RestController
 public class LivrosResources {
 	
 	@RequestMapping(value = "/livros", method = RequestMethod.GET)
-	public String listar() {
-		return "Java Avançado, Softblue, Algaworks";
+	public List<Livro> listar() {
+		
+		Livro l1 = new Livro("Java Avançado");
+		Livro l2 = new Livro("Softblue");
+		Livro l3 = new Livro("Algaworks");
+		
+		Livro[] livros = {l1,l2,l3};
+		
+		return Arrays.asList(livros);
 	}
 }
